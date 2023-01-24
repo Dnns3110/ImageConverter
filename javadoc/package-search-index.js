5b51f7a7e99384463936e6283d417301b9ad0d02
dnns01 <github@dnns01.de>
2023-01-24 14:05:05 +0100

    Remove javadoc due to security issues

--- javadoc/package-search-index.js
+++
@@ -1,4 +0,0 @@
-packageSearchIndex = [{
-    "l": "All Packages",
-    "url": "allpackages-index.html"
-}, {"l": "propra.imageconverter"}, {"l": "propra.imageconverter.exceptions"}, {"l": "propra.imageconverter.handler"}, {"l": "propra.imageconverter.image"}, {"l": "propra.imageconverter.io"}, {"l": "propra.imageconverter.rlepacket"}]
